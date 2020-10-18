b9eecc4118b6093424d11e63137c85d732ad76c6
altanege <altanege.1@gmail.com>
2020-10-18 08:47:20 +0200

    added comments on same lien

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Still really awesome hello");
+
+        // add one more line on your local and commit
+
+        System.out.println("Added new commit");
     }
 }
