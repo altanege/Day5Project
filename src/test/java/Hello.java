fe28522a8f38036efd9a966e6d7dec53f56f8b1a
altanege <62395900+altanege@users.noreply.github.com>
2020-10-18 08:55:01 +0200

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello");
+        System.out.println("Still really My Coworker added awesome hello"); // we are simulating the situation my cooworker made the changes
 
         // add one more line on your local and commit
 
